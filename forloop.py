7c2690d836c17d5b0d556ee281179d578276cd83
Ghost <avs.adh96@gmail.com>
2017-09-19 22:06:24 +0545

    no comments

---
+++ forloop.py
@@ -0,0 +1,8 @@
+foods=['tuna','beef','sausages','momo']
+for f in foods:#each item in foods is treated at f..loopes through entire list one item at a time
+    print(f)
+    print(len(f))
+
+for f in foods[:2]:#each item in foods is treated at f..loopes through 1th item one item at a time
+    print(f)
+    print(len(f))
---
+++ ifelse.py
@@ -0,0 +1,14 @@
+age=21
+if age==21: #Notice the use of : , indendatioan and no braces
+    print("No beer for u")
+
+name="Goku"
+if name is "Avash": #notice the use of 'is'
+    print("Hey there Avash")
+elif name is "Lucy": #notice the syntax of elseif
+    print("What up luce")
+elif name is "Toto":
+    print("Tyau Tyau")
+else:
+    print("No name matches")#this runs after none of the conditions are true
+
