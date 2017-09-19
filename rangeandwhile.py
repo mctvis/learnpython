c4b9386f022bf8c1bee4595ef65e3ea523e860b8
Ghost <avs.adh96@gmail.com>
2017-09-19 22:20:19 +0545

    no comments

---
+++ rangeandwhile.py
@@ -0,0 +1,12 @@
+for x in range(10): # x's value is 0-9
+    print (x)
+for x in range(5,12): #x's value is 5-11
+    print(x)
+for x in range(5,17,3): #third param is increment Note here 17 is not printed
+    print(x)
+
+avash=5
+while avash<10: #keep looping until this condition becomes false
+    print('avashsamosa');
+    avash+=1
+
