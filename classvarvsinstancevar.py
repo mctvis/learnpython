45d5c768d2fd26283f7275f483d27264b6d5c5f7
Ghost <avs.adh96@gmail.com>
2017-09-22 17:14:18 +0545

    no comments

---
+++ classvarvsinstancevar.py
@@ -0,0 +1,10 @@
+class Girl:
+    gender="female" #this is class  common for all girls
+
+    def __init__ (self,name):
+        self.name=name #this is instance variable which is gonna be different for each girl
+
+r=Girl('Rachel')
+s=Girl('Rachel')
+print(r.gender)
+print(r.gender)
