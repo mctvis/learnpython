3a027fd4e37021ca65115f08553b524b1fe070a7
Ghost <avs.adh96@gmail.com>
2017-12-13 21:22:39 +0545

    no comments

--- callfromanimal.py
+++ callfromanimal.py
@@ -1,7 +1,7 @@
 #using function from animal.py
 import animal
 import random #includes bunch of functions python library.. You can also download other moudles from internet
-animal.fish() #notice the use of animal
+animal.fish() #notice the use of animal.
 
 x=random.randrange(1,2000) #generate a random number from 1 to 1000
 print(x)
