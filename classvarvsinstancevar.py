b78012865c3b8caaf0e00c1e19de6a5593107ccc
Ghost <avs.adh96@gmail.com>
2017-12-13 21:21:17 +0545

    no comments

--- classvarvsinstancevar.py
+++ classvarvsinstancevar.py
@@ -3,8 +3,12 @@ class Girl:
 
     def __init__ (self,name):
         self.name=name #this is instance variable which is gonna be different for each girl
+    def gal(self):
+        print('I am a gal')
 
 r=Girl('Rachel')
+rg=r.gal
+rg()
 s=Girl('Habibi')
 print(r.name)
 print(s.name)
